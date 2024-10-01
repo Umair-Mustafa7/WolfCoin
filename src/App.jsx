099c2d93489e6ecc3131b43af302760b9bf9076e
Umair-Mustafa7 <15wrestlingfacts@gmail.com>
2024-10-01 11:53:43 +0500

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Tasks from "./components/Tasks";
