d3471876e6d8334eb8f6c5f1bc9145c86a1e615c
Umair-Mustafa7 <15wrestlingfacts@gmail.com>
2024-10-01 12:09:47 +0500

    xc

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
+  resolve: {
+    alias: {
+      '@': '/src', // example alias setup
     },
-    outDir: 'dist',  // Ensure the output directory is set to 'dist' (default for Vite)
-    sourcemap: true,  // Helpful for debugging production issues
   },
-  
 });
